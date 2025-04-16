49163123105c79d5495e835c0ff7a192c2fab52e
Direcub <bradleygalatas@gmail.com>
2025-04-15 23:46:12 -0400

    made changes up to CH2 L4

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
+	"io"
+	"log"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +35,75 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
+	file, header_pointer, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "failure in getting file", err)
+	}
+	defer file.Close()
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	mediaType := header_pointer.Header.Get("Content-Type")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to convert file", err)
+		return
+	}
+	videoData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error Fetching Video", err)
+		return
+	}
+	if videoData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User not authorized", err)
+	}
+
+	mime, _, _ := mime.ParseMediaType(mediaType)
+
+	fileType := ""
+	switch mime {
+	case "image/png":
+		fileType = ".png"
+	case "image/jpg":
+		fileType = ".jpg"
+	case "image/jpeg":
+		fileType = ".jpeg"
+	default:
+		respondWithError(w, 500, "invalid media type", nil)
+		return
+	}
+
+	byteSlice := make([]byte, 32)
+	rand.Read(byteSlice)
+	randName := base64.RawURLEncoding.EncodeToString(byteSlice)
+
+	thumbName := randName + fileType
+	filePath := filepath.Join(cfg.assetsRoot, thumbName)
+	log.Printf("filePath: %v", filePath)
+
+	filePointer, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "failure in getting file", err)
+		return
+	}
+	defer filePointer.Close()
+
+	_, err = io.Copy(filePointer, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "failure in getting file", err)
+		return
+	}
+
+	thumbnailURLPath := "/assets/" + thumbName
+
+	videoData.ThumbnailURL = &thumbnailURLPath
+
+	err = cfg.db.UpdateVideo(videoData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to update video metadata", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoData)
 }
